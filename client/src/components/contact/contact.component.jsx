b30d63c85e7a35067c1a8d528da259e66653e02b
Vanessa Martin <vmartin@altair.com>
2020-02-20 16:49:54 -0800

    completed contact form functionality, emails are sending to l0tusart007@gmail.com

--- client/src/components/contact/contact.component.jsx
+++ client/src/components/contact/contact.component.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import * as emailjs from "emailjs-com";
+
 import "./contact.styles.css";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
@@ -18,11 +20,41 @@ class Contact extends Component {
   };
 
   handleSubmit = e => {
-      e.preventDefault()
-  }
+    const { name, email, message } = this.state;
+    e.preventDefault();
+
+    const messageUpdated = `From | ${name}: ` + message;
+
+    const templateParams = {
+      from_name: email,
+      to_name: "l0tusart007@gmail.com",
+      subject: "Lotus Art",
+      message_html: messageUpdated
+    };
+
+    emailjs
+      .send(
+        "gmail",
+        "template_mgKZRCOz",
+        templateParams,
+        "user_ZGzQ6szqqXtsDwYcf6Edg"
+      )
+      .then(response => {
+        alert("Message sent");
+        this.resetForm();
+      })
+      .catch(error => {
+        console.log("Message Error: ", error);
+        alert("There was an issue with your message! Please try again.");
+      });
+  };
+
+  resetForm = () => {
+    this.setState({ name: "", email: "", message: "" });
+  };
 
   render() {
-      const {email, name, message} = this.state
+    const { email, name, message } = this.state;
     return (
       <div className="contact-container">
         <h1>Have questions? Email me!</h1>
@@ -33,22 +65,25 @@ class Contact extends Component {
             value={email}
             onChange={this.handleChange}
             label="Email"
-            required />
+            required
+          />
           <FormInput
             type="text"
             name="name"
             value={name}
             onChange={this.handleChange}
             label="Name"
-            required />
+            required
+          />
           <FormInput
             type="text"
             name="message"
             value={message}
             onChange={this.handleChange}
             label="Message"
-            required />
-            <CustomButton type="submit"> CONTACT </CustomButton>
+            required
+          />
+          <CustomButton type="submit"> CONTACT </CustomButton>
         </form>
       </div>
     );
