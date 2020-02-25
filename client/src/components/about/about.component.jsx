3c914100bd017f9ba35300aab901af3ab94fd602
Vanessa Martin <vmartin@altair.com>
2020-02-25 11:06:03 -0800

    adding about

--- client/src/components/about/about.component.jsx
+++ client/src/components/about/about.component.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import './about.styles.css'
+import React from "react";
+import "./about.styles.css";
 
 const About = () => {
-    return (
-        <div className="about-container">
-            <h1>About</h1>
-        </div>
-    )
-}
+  return (
+    <div className="about-container">
+      <h1>About</h1>
+      <div className="about-content">
+      <p>
+        As an Oakland-native born during the tech boom in Silicon Valley,
+        Vanessa has always been interested in technology and has been working on
+        computers for as long as she can remember. She learned that through
+        code, she could create whatever she dreamt of. Even more, she loves how
+        self-reliant coding makes you. Whether your code works or not, is up to
+        you. Vanessa, seeing the similarities between coding and art, decided to
+        pursue and master both.
+      </p>
+      <br/>
+      <p>
+        Vanessa is a Full-stack Web Developer and Artist. Her speciality is
+        JavaScript, which is the language that predominantly controls the
+        internet. She also freelances and offers speaking engagements and
+        mentorship. Her career has been diverse, from working with e-commerce
+        start-ups to major international coding companies.
+      </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
